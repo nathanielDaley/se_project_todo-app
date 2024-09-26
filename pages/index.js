1d705822158df00346b6b898453ca8b614f756db
Nathaniel Daley <ndaley64@gmail.com>
2024-09-25 19:36:03 -0500

    refactor: create Section class and use it to generate and add todos

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,21 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import Section from "../components/Section.js";
 import FormValidator from "../components/FormValidator.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-const todosList = document.querySelector(".todos__list");
+
+const todosList = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    renderTodo(item);
+  },
+  containerSelector: ".todos__list",
+});
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
 
@@ -20,7 +28,6 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-// The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
@@ -30,7 +37,7 @@ const generateTodo = (data) => {
 
 const renderTodo = (item) => {
   const todo = generateTodo(item);
-  todosList.append(todo);
+  todosList.addItem(todo);
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -59,6 +66,6 @@ addTodoForm.addEventListener("submit", (evt) => {
   closeModal(addTodoPopup);
 });
 
-initialTodos.forEach(renderTodo);
+todosList.renderItems();
 
 newTodoValidator.enableValidation();
